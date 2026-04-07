11cb6fb5e72eca569030d2d60f10cf60ff3ae204
Asko Soukka <asko.soukka@iki.fi>
2026-04-07 08:03:52 +0000

    Routes & content: finalize route fixes and add site content config + CI/workflows

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -11,11 +11,8 @@ import Frontmatter exposing (Frontmatter)
 import Head
 import Head.Seo as Seo
 import Json.Decode as Decode
-import LanguageTag.Language
-import LanguageTag.Region
 import MarkdownRenderer
 import Metadata
-import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import RouteBuilder exposing (App, StatelessRoute)
 import Shared
@@ -78,21 +75,56 @@ head app =
 
         site =
             app.sharedData.config.site
+
+        metadata =
+            app.sharedData.config.metadata
+
+        seoTitle =
+            fm.title
+
+        seoDescription =
+            fm.description
+
+        canonicalUrl =
+            Metadata.pageCanonicalUrl site.url "/"
+
+        pageAuthor =
+            case fm.author of
+                Just author ->
+                    Just author
+
+                Nothing ->
+                    metadata.author
+
+        baseSeo =
+            { canonicalUrlOverride = Just canonicalUrl
+            , siteName = site.title
+            , image =
+                Metadata.socialImage
+                    { defaultImageAlt = metadata.defaultSocialImageAlt
+                    , defaultImagePath = metadata.defaultSocialImage
+                    , maybeImageAlt = fm.imageAlt
+                    , maybeImagePath = fm.image
+                    , siteUrl = site.url
+                    }
+            , description = seoDescription
+            , locale = Nothing
+            , title = seoTitle
+            }
     in
-    (Seo.summaryLarge
-        { canonicalUrlOverride = Nothing
-        , siteName = site.title
-        , image = Metadata.socialImage site.url fm.title fm.image
-        , description = fm.description
-        , locale = Just ( LanguageTag.Language.fi, LanguageTag.Region.fi )
-        , title = fm.title
-        }
+    ((if metadata.twitterCard == "summary" then
+        Seo.summary baseSeo
+
+      else
+        Seo.summaryLarge baseSeo
+     )
         |> Seo.website
     )
+        ++ Metadata.maybeMetaName "author" pageAuthor
         ++ [ Metadata.webPageStructuredData
-                { title = fm.title
-                , description = fm.description
-                , url = site.url
+                { title = seoTitle
+                , description = seoDescription
+                , url = canonicalUrl
                 }
            ]
 
